f9d52bcce2f75862fe3998f8543ab51e3512ce67
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-04-21 19:22:43 +0600

    dowpdown design and make

--- src/Component/Navbars/Navbars.jsx
+++ src/Component/Navbars/Navbars.jsx
@@ -1,62 +1,125 @@
 import { Container, Navbar } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 
 function NavbarNav() {
   return (
-   
-        <div className='div_one_background'>
-        <Navbar collapseOnSelect expand="lg" className="navbar_style">
-      <Container className="navbar_container">
-        <Navbar.Brand>          
-          <NavLink to="/home" className="">
-            <img src="/Images/LOGO.png" alt="logo" />
-          </NavLink>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="">
-            <NavLink to="/home" className="nav_text">
-              Home
+    <div className="div_one_background">
+      <Navbar collapseOnSelect expand="lg" className="navbar_style">
+        <Container className="navbar_container">
+          <Navbar.Brand>
+            <NavLink to="/home" className="">
+              <img src="/Images/LOGO.png" alt="logo" />
             </NavLink>
-            <NavLink to="/our-team" className="nav_text">
-            About Us
-            </NavLink>
-            <NavDropdown
-              title="Services"
-              id="collapsible-nav-dropdown"
-              className="nav_text dropdown_menu_text"
-            >
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="">
+              <NavLink to="/home" className="nav_text">
+                Home
+              </NavLink>
               <NavLink to="/our-team" className="nav_text">
-              Our Team
-            </NavLink>
-              
-            </NavDropdown>
-            
-            <NavLink to="/gallery" className="nav_text">
-            Clients
-            </NavLink>
-            <NavLink to="/blog" className="nav_text">
-            Company
-            </NavLink>
-            <NavLink to="/contact" className="nav_text">
-            Blog
-            </NavLink>
+                About Us
+              </NavLink>
 
-            <NavLink to="/contact" >
-            
+              {/* demo  */}
+
+<nav className="menu_bar nav_text">
+  <ul>
+    <li className="menu_item">
+      <span className="services">
+        Services <IoIosArrowDown />
+      </span>
+      <div className="dropdown_menu">
+        <ul className="submenu">
+          <li>
+            <span className="services">
+              Package <IoIosArrowForward className="fas" />
+            </span>
+            <div className="dropdown_menu_1">
+              <ul>
+                {Array.from({ length: 5 }, (_, index) => (
+                  <li key={index}>
+                    <NavLink to="/our-team" className="dropdown-item">
+                      PackagePackagePackagePackagePackagePackage
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <span className="services">
+            Management <IoIosArrowForward className="fas" />
+            </span>
+            <div className="dropdown_menu_1">
+              <ul>
+                {Array.from({ length: 5 }, (_, index) => (
+                  <li key={index}>
+                    <NavLink to="/our-team" className="dropdown-item">
+                    ManagementManagementManagementManagementManagement
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+          <li>
+            <NavLink to="/management" className="dropdown-item">
+              Management
             </NavLink>
-          </Nav>
-          <div>
-          <button type="button" className="contactBtn">Contact Us</button>
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-        </div>
-      
-    
+          </li>
+          <li>
+            <NavLink to="/marketing" className="dropdown-item">
+              Marketing
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/development" className="dropdown-item">
+              Development
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/design" className="dropdown-item">
+              Design
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/listing" className="dropdown-item">
+              Listing
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </li>
+  </ul>
+</nav>
+
+              
+
+              <NavLink to="/gallery" className="nav_text">
+                Clients
+              </NavLink>
+              <NavLink to="/blog" className="nav_text">
+                Company
+              </NavLink>
+              <NavLink to="/contact" className="nav_text">
+                Blog
+              </NavLink>
+
+              <NavLink to="/contact"></NavLink>
+            </Nav>
+            <div>
+              <button type="button" className="contactBtn">
+                Contact Us
+              </button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
