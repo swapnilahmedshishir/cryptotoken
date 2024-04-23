7541082b4c7a3cf2061c53dabfaa0feec1d20ddd
SWAPNIL AHMED SHISHIR <ahmed66882298@gmail.com>
2024-04-23 15:08:27 +0600

    home hero part codding done

--- src/Component/HomePage/HomePage.jsx
+++ src/Component/HomePage/HomePage.jsx
@@ -1,9 +1,20 @@
+import { Link } from "react-router-dom"
+
 const HomePage = () => {
   return (
     <>
-     <div>
+     <div className="hero_div">
         <div className='container'>
-        HomePage
+        <div className="row hero_row_style">
+    <div className="col-sm-12 col-md-6 hero_col_text_style">
+      <strong>Empowering Industries for success with TOJO: Unlocking maximum potential and growth</strong>
+      <p>Wellcome to TOJO Global! We specialize in providing professional services of marketing, management, designing, developments,listing and many more for startups, small businesses, B2B companies, cryptocurrency, blockchain,fintech etc. Let&#39;s work together to move your company forward.</p>
+      <Link to='/'><button className='custombtn'>  GET STARTED </button></Link>
+    </div>
+    <div className="col-sm-12 col-md-6 hero_col_image_style ">
+      <img src="Images/Hero-01.png" alt="" className="animationY_style"/>
+    </div>
+    </div>
         </div>
       </div>
       
